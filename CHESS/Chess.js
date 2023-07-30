ebfa1e80c0af0179d5d5532330c8a30da58d332b
Jahid Khan <98642084+jahid28@users.noreply.github.com>
2023-07-30 15:41:38 +0530

    Add files via upload

--- CHESS/Chess.js
+++ CHESS/Chess.js
@@ -21,7 +21,6 @@ function insertImage() {
 insertImage()
 
 
-
 //Coloring
 
 function coloring() {
@@ -42,6 +41,12 @@ function coloring() {
         if (a % 2 !== 0) {
             color.style.backgroundColor = 'rgb(100, 75, 43)'
         }
+        // if (a % 2 == 0) {
+        //     color.style.backgroundColor = 'seagreen'
+        // }
+        // if (a % 2 !== 0) {
+        //     color.style.backgroundColor = 'lime'
+        // }
 
     })
 }
@@ -74,7 +79,7 @@ function reddish() {
                     aside = eval(arr.pop())
                     aup = eval(arr.shift())
                     a = aside + aup
-            
+
                     if (a % 2 == 0 && pinkColor == greenColor) {
                         i2.style.backgroundColor = 'rgb(240, 201, 150)'
                     }
@@ -101,6 +106,8 @@ function reddish() {
 
 
 tog = 1
+whiteCastleChance=true
+blackCastleChance=true
 
 document.querySelectorAll('.box').forEach(item => {
 
@@ -111,6 +118,9 @@ document.querySelectorAll('.box').forEach(item => {
         // To delete the opposite element
 
         if (item.style.backgroundColor == 'green' && item.innerText.length == 0) {
+            tog = tog + 1
+        }
+        else if (item.style.backgroundColor == 'aqua' && item.innerText.length == 0) {
             tog = tog + 1
         }
 
@@ -126,7 +136,7 @@ document.querySelectorAll('.box').forEach(item => {
                     coloring()
                     insertImage()
                     tog = tog + 1
-                    
+
                 }
             })
         }
@@ -154,7 +164,14 @@ document.querySelectorAll('.box').forEach(item => {
 
                 if (tog % 2 !== 0 && aup < 800) {
 
-                    if (document.getElementById(`b${a + 100}`).innerText.length == 0) {
+                    if (aup == 200 && document.getElementById(`b${a + 100}`).innerText.length == 0) {
+                        document.getElementById(`b${a + 100}`).style.backgroundColor = 'green'
+                        if (aup == 200 && document.getElementById(`b${a + 200}`).innerText.length == 0) {
+                            document.getElementById(`b${a + 200}`).style.backgroundColor = 'green'
+                        }
+                    }
+
+                    if (aup !== 200 && document.getElementById(`b${a + 100}`).innerText.length == 0) {
                         document.getElementById(`b${a + 100}`).style.backgroundColor = 'green'
                     }
 
@@ -166,11 +183,31 @@ document.querySelectorAll('.box').forEach(item => {
                         document.getElementById(`b${a + 100 - 1}`).style.backgroundColor = 'green'
 
                     }
+                    // if (aup == 800) {
+                    //     document.getElementById(`b${a}`).innerText = 'Wqueen'
+                    //     coloring()
+                    //     insertImage()
+                    // }
+                    // if (aside < 8 && document.getElementById(`b${a + 100 + 1}`).innerText.length == 0 && document.getElementById(`b${a + 100}`).innerText.length == 0) {
+                    //     document.getElementById(`b${a + 100}`).style.backgroundColor = 'green'
+                    // }
+
+                    // if (aside > 1 && document.getElementById(`b${a + 100 - 1}`).innerText.length == 0 && document.getElementById(`b${a + 100}`).innerText.length == 0) {
+                    //     document.getElementById(`b${a + 100}`).style.backgroundColor = 'green'
+
+                    // }
                 }
 
                 if (tog % 2 == 0 && aup > 100) {
 
-                    if (document.getElementById(`b${a - 100}`).innerText.length == 0) {
+                    if (aup == 700 && document.getElementById(`b${a - 100}`).innerText.length == 0) {
+                        document.getElementById(`b${a - 100}`).style.backgroundColor = 'green'
+                        if (aup == 700 && document.getElementById(`b${a - 200}`).innerText.length == 0) {
+                            document.getElementById(`b${a - 200}`).style.backgroundColor = 'green'
+                        }
+                    }
+
+                    if (aup !== 700 && document.getElementById(`b${a - 100}`).innerText.length == 0) {
                         document.getElementById(`b${a - 100}`).style.backgroundColor = 'green'
                     }
                     if (aside < 8 && document.getElementById(`b${a - 100 + 1}`).innerText.length !== 0) {
@@ -222,6 +259,23 @@ document.querySelectorAll('.box').forEach(item => {
                 if (aup < 800 && aside > 1) {
 
                     document.getElementById(`b${a + 100 - 1}`).style.backgroundColor = 'green'
+                }
+                
+                if(whiteCastleChance==true && a==105 && document.getElementById('b106').innerText== '' && document.getElementById('b107').innerText== '' && document.getElementById('b108').innerText== 'Wrook'){
+                    document.getElementById(`b107`).style.backgroundColor = 'aqua'
+
+                }
+                if(whiteCastleChance==true && a==105 && document.getElementById('b104').innerText== '' && document.getElementById('b103').innerText== '' && document.getElementById('b102').innerText== '' && document.getElementById('b101').innerText== 'Wrook'){
+                    document.getElementById(`b103`).style.backgroundColor = 'aqua'
+
+                }
+                if(blackCastleChance==true && a==805 && document.getElementById('b806').innerText== '' && document.getElementById('b807').innerText== '' && document.getElementById('b808').innerText== 'Brook'){
+                    document.getElementById(`b807`).style.backgroundColor = 'aqua'
+
+                }
+                if(blackCastleChance==true && a==805 && document.getElementById('b804').innerText== '' && document.getElementById('b803').innerText== '' && document.getElementById('b802').innerText== '' && document.getElementById('b801').innerText== 'Brook'){
+                    document.getElementById(`b803`).style.backgroundColor = 'aqua'
+
                 }
 
                 item.style.backgroundColor = 'pink'
@@ -528,24 +582,105 @@ document.querySelectorAll('.box').forEach(item => {
 
 
 // Moving the element
-document.querySelectorAll('.box').forEach(hathiTest => {
+document.querySelectorAll('.box').forEach(item => {
 
-    hathiTest.addEventListener('click', function () {
+    item.addEventListener('click', function () {
 
-        if (hathiTest.style.backgroundColor == 'pink') {
 
-            pinkId = hathiTest.id
-            pinkText = hathiTest.innerText
+        if (item.style.backgroundColor == 'pink') {
 
-            document.querySelectorAll('.box').forEach(hathiTest2 => {
+            pinkId = item.id
+            pinkText = item.innerText
 
-                hathiTest2.addEventListener('click', function () {
-                    if (hathiTest2.style.backgroundColor == 'green' && hathiTest2.innerText.length == 0) {
-                        document.getElementById(pinkId).innerText = ''
-                        hathiTest2.innerText = pinkText
-                        coloring()
-                        insertImage()
+            document.querySelectorAll('.box').forEach(item2 => {
 
+                item2.addEventListener('click', function () {
+
+                    getId = item2.id
+                    arr = Array.from(getId)
+                    arr.shift()
+                    aside = eval(arr.pop())
+                    arr.push('0')
+                    aup = eval(arr.join(''))
+                    a = aside + aup
+
+                    if (item2.style.backgroundColor == 'green' && item2.innerText.length == 0) {
+
+                        if (pinkText == `Wpawn` && aup == 800) {
+
+                            document.getElementById(`b${a}`).innerText = 'Wqueen'
+                            document.getElementById(pinkId).innerText = ''
+                            coloring()
+                            insertImage()
+
+                        }
+                        else if (pinkText == `Bpawn` && aup == 100) {
+
+                            document.getElementById(`b${a}`).innerText = 'Bqueen'
+                            document.getElementById(pinkId).innerText = ''
+                            coloring()
+                            insertImage()
+
+                        }
+                        else {
+
+
+
+                            document.getElementById(pinkId).innerText = ''
+                            item2.innerText = pinkText
+                            coloring()
+                            insertImage()
+                        }
+
+                    }
+
+                    else if (item2.style.backgroundColor == 'aqua') {
+                        if(item2.id=='b103'){
+                            document.getElementById('b101').innerText = ''
+                            document.getElementById('b102').innerText = ''
+                            document.getElementById('b103').innerText = 'Wking'
+                            document.getElementById('b104').innerText = 'Wrook'
+                            document.getElementById('b105').innerText = ''
+                            document.getElementById(pinkId).innerText = ''
+                            whiteCastleChance=false
+                            coloring()
+                            insertImage()
+                            
+                        }
+                        else if(item2.id=='b107'){
+                            document.getElementById('b105').innerText = ''
+                            document.getElementById('b106').innerText = 'Wrook'
+                            document.getElementById('b107').innerText = 'Wking'
+                            document.getElementById('b108').innerText = ''
+                            document.getElementById(pinkId).innerText = ''
+                            whiteCastleChance=false
+                            coloring()
+                            insertImage()
+
+                        }
+                        else if(item2.id=='b803'){
+                            document.getElementById('b801').innerText = ''
+                            document.getElementById('b802').innerText = ''
+                            document.getElementById('b803').innerText = 'Bking'
+                            document.getElementById('b804').innerText = 'Brook'
+                            document.getElementById('b805').innerText = ''
+                            document.getElementById(pinkId).innerText = ''
+                            blackCastleChance=false
+                            coloring()
+                            insertImage()
+                            
+                        }
+                        else if(item2.id=='b807'){
+                            document.getElementById('b805').innerText = ''
+                            document.getElementById('b806').innerText = 'Brook'
+                            document.getElementById('b807').innerText = 'Bking'
+                            document.getElementById('b808').innerText = ''
+                            document.getElementById(pinkId).innerText = ''
+                            blackCastleChance=false
+                            coloring()
+                            insertImage()
+
+                        }
                     }
 
                 })
@@ -567,7 +702,7 @@ z = 0
 document.querySelectorAll('.box').forEach(ee => {
     ee.addEventListener('click', function () {
         z = z + 1
-        if (z % 2 == 0 && ee.style.backgroundColor !== 'green') {
+        if (z % 2 == 0 && ee.style.backgroundColor !== 'green' && ee.style.backgroundColor !== 'aqua') {
             coloring()
         }
     })
